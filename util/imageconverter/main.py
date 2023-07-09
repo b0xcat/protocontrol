6434b7c07fe795cc94816d53bd6686c487e603f9
b0xcat <me@b0xcat.nl>
2023-07-09 15:44:25 +0200

    Automatically call imageconverter script if using target uploadfs

--- util/imageconverter/main.py
+++ util/imageconverter/main.py
@@ -1,19 +1,39 @@
-from pathlib import Path
-from PIL import Image
+Import("env")
+env.Execute("$PYTHONEXE -m pip --version")
 
+from pathlib import Path
+from inspect import getsourcefile
+from os.path import abspath
+
+try:
+    from PIL import Image
+except ImportError:
+    env.Execute("$PYTHONEXE -m pip install Pillow")
+    from PIL import Image
+
+here = Path(abspath(getsourcefile(lambda:0))).parent
 
 def RGBto565(r, g, b):
     # return bytes.fromhex("%0.4X" % ((int(b / 255 * 31) << 11) | (int(r / 255 * 63) << 5) | (int(g / 255 * 31))))
     return bytes.fromhex(f"{(((r & 0b11111000)<<8) + ((g & 0b11111100)<<3)+(b>>3)):04x}")
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+def main(source, target, env):
+    print("Converting input pngs to 565 format")
+
     # Ensure out directory exists
-    out_dir = Path("out")
-    out_dir.mkdir(exist_ok=True)
+    out_dir = (here / '../../data/565').resolve()
+    print("out dir:", out_dir)
+    assert out_dir.is_dir()
+
+    # Ensure in directory exists
+    in_dir = (here / Path("in")).resolve()
+    print("in dir:", in_dir)
+    assert in_dir.is_dir()
 
     # Loop over all files in input directory
-    for path in [x for x in Path("in").glob("*.png") if x.is_file()]:
+    for path in [x for x in (here / Path("in")).glob("*.png") if x.is_file()]:
         print(f"Converting {path}")
 
         try:
@@ -45,3 +65,5 @@ if __name__ == "__main__":
         out_path = out_dir / Path(f"{path.stem}")
         with out_path.open('wb') as file:
             file.write(out_bytes)
+
+env.AddPreAction("uploadfs", main)
